ecaa642f6c2573e2764349a55342684cace85db4
Jacek Chleborowicz <j.chleborowicz@gmail.com>
2023-08-17 15:28:12 +0200

    Initial commit.

---
+++ src/main/kotlin/com/example/springkotlingatgetgallery/SpringKotlinGatgetGalleryApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springkotlingatgetgallery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinGatgetGalleryApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringKotlinGatgetGalleryApplication>(*args)
+}
